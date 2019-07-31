a613f0a6e54ebea723af4e204767f6d50bb71284
quals <xionglinlin2@gmail.com>
2019-07-31 21:28:04 +0800

    完成了p151 reverse_words_in_string

---
+++ p151_reverse_words_in_string/main.cpp
@@ -0,0 +1,48 @@
+//
+// Created by quals on 2019-07-31.
+//
+
+#include <string>
+#include <iostream>
+#include <unistd.h>
+
+using namespace std;
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        int j;
+        int len = s.length();
+        const char *str = s.data();
+        string res;
+        for (int i = len - 1; i >= 0;i--) {
+            for (; str[i] == ' ' && i; i--);
+            j = i;
+            for (; str[i] != ' ' && i; i--);
+            if (i == j && str[i] == ' ')break;
+            res.append(s.substr(i + (str[i] == ' '), j - i + (str[i] != ' ')));
+            res.append(" ");
+        }
+        if (res.length() > 1)
+            res.erase(res.length() - 1);
+        return res;
+    }
+};
+
+Solution solution;
+
+void test(string input, string ans) {
+    cout << "input: " << input << endl;
+    cout << "output: " << solution.reverseWords(input) << endl;
+    cout << "ans: " << ans << endl;
+    cout << endl;
+}
+
+int main() {
+    test("the sky is blue", "blue is sky the");
+    test("  hello world!  ", "world! hello");
+    test("a good   example", "example good a");
+    test("", "");
+    test("a","a");
+    test("hello","hello");
+}
